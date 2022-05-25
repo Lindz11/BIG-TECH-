1763064b54cde05a0076a3f8a98ebbd7af1e5a19
Lindz11 <lindsaygriffith@knights.ucf.edu>
2022-05-24 20:35:16 -0400

    Create ContainsDuplicate.java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> dup = new HashSet<>(); 
+        
+        for(int n: nums){
+            if(!dup.add(n))
+                return true; 
+        }
+        
+        return false; 
+    }
+}
